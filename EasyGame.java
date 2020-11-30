2c3ddc0814327f8267ad43ea6527f3acde5d0fab
arrector <41345883+arrector@users.noreply.github.com>
2020-11-30 03:45:58 -0500

    Add files via upload

--- EasyGame.java
+++ EasyGame.java
@@ -10,5 +10,7 @@ public class EasyGame extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_easy_game);
+
+        Engine.getInstance().createGrid(this);
     }
 }
